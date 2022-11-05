90a8eaaeb54db59853ccd011300eef7b8fc7a49a
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-11-05 12:54:49 -0400

    Setup GL render loop

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,83 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+// Debug message handler
+void GLAPIENTRY messageCallback(GLenum source,
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei length,
+    const GLchar* message,
+    const void* userParam) {
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+        (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+        type, severity, message);
+}
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+    std::string title = "3D Snake";
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    glm::i32vec2 prevWindowSize{ -1, -1 };
+    glm::i32vec2 windowSize{ 1280, 720 };
+
+    GLFWwindow* window = glfwCreateWindow(windowSize.x, windowSize.y, title.c_str(), NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    glewInit();
+
+    // Double buffered V-Sync
+    glfwSwapInterval(1);
+    glfwWindowHint(GLFW_DOUBLEBUFFER, true);
+
+    // Mouse/Keyboard callbacks
+    //glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    //glfwSetKeyCallback(window, keyCallback);
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(messageCallback, 0);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glEnable(GL_CULL_FACE);
+
+    // Setup here
+
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+        std::cerr << "Pre render OpenGL error: " << err << std::endl;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        // Process inputs here
+
+        glfwGetWindowSize(window, &windowSize.x, &windowSize.y);
+
+        if (prevWindowSize != windowSize && windowSize.x != 0 && windowSize.y != 0) {
+            prevWindowSize = windowSize;
+        }
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glViewport(0, 0, windowSize.x, windowSize.y);
+
+        // Render goes here
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        GLenum err;
+        while ((err = glGetError()) != GL_NO_ERROR) {
+            std::cerr << "OpenGL error: " << err << std::endl;
+        }
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
 }
