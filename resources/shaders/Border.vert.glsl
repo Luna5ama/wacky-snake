c8fc629c03335d668da59b5236a0a8d65cfd3c3c
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-11-05 22:59:37 -0400

    Better border rendering

--- resources/shaders/Border.vert.glsl
+++ resources/shaders/Border.vert.glsl
@@ -11,10 +11,13 @@ layout(std140) uniform Global {
 
 layout(location = 0) in vec3 pos;
 
+out vec3 rawPos;
 out vec4 color;
 
 void main() {
     vec4 renderPosition = modelView * vec4(pos, 1.0);
 	gl_Position = projection * renderPosition;
-    color = vec4(pos / 256.0 + 0.5, 0.2 + 0.4 * smoothstep(2.0, 0.0, length(renderPosition)));
+    vec3 temp = pos / 256.0 + 0.5;
+    rawPos = pos;
+    color = vec4(1.0 - temp.r, temp.g, 0.75 * temp.b + 0.25, 0.4 * smoothstep(2.0, 0.0, length(renderPosition)));
 }
