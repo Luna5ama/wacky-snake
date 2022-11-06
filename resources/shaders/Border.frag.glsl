2a9d574da2ba02ba07ddbe623f74bc721f3fabe8
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-11-05 22:59:37 -0400

    Better border rendering

--- resources/shaders/Border.frag.glsl
+++ resources/shaders/Border.frag.glsl
@@ -1,10 +1,11 @@
 #version 460
 
 in vec4 color;
-in float alpha;
+in vec3 rawPos;
 
 out vec4 fragColor;
 
 void main() {
 	fragColor = color;
+	fragColor.a += 0.5 * smoothstep(100.0, 215.0, length(rawPos));
 }
